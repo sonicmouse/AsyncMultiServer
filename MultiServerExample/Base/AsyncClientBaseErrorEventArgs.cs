b4778507958ed314d08f6e5f35d100061e309dd3
Andrew Heinlein <heinlein@dciartform.com>
2020-09-26 18:19:24 -0500

    initial commit

---
+++ MultiServerExample/Base/AsyncClientBaseErrorEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultiServerExample.Base
+{
+	public class AsyncClientBaseErrorEventArgs : EventArgs
+	{
+		public AsyncClientBaseErrorEventArgs(Exception ex, string message)
+		{
+			Message = message;
+			Exception = ex;
+		}
+
+		public Exception Exception { get; private set; }
+
+		public string Message { get; private set; }
+	}
+}
---
+++ MultiServerExample/Implementation/MyOtherClient.cs
@@ -0,0 +1,30 @@
+﻿using MultiServerExample.Base;
+using System;
+
+namespace MultiServerExample.Implementation
+{
+	public class MyOtherClient : AsyncClientBase
+	{
+		public override void OnAttachedToServer()
+		{
+			base.OnAttachedToServer();
+
+			Console.WriteLine($"{Id}: {GetType().Name} attached. Writing 4 bytes back.");
+			WriteData(new byte[] { 1, 2, 3, 4 });
+		}
+
+		public override void OnDataReceived(byte[] buffer)
+		{
+			base.OnDataReceived(buffer);
+
+			Console.WriteLine($"{Id}: {GetType().Name} recieved {buffer.Length} bytes.");
+		}
+
+		public override void OnDetachedFromServer(Exception ex)
+		{
+			base.OnDetachedFromServer(ex);
+
+			Console.WriteLine($"{Id}: {GetType().Name} detached.");
+		}
+	}
+}
